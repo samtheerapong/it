779ffd3e67c69918732d785a3a5e1ccdfd043a25
samtheerapong <theerapong.khan@gmail.com>
2024-02-10 22:48:51 +0700

    up

--- config/db.php
+++ config/db.php
@@ -1,13 +1,8 @@
 <?php
 
 return [
-    // 'class' => 'yii\db\Connection',
-    // 'dsn' => 'mysql:host=localhost;dbname=db-it',
-    // 'username' => 'sam',
-    // 'password' => 'sam',
-    // 'charset' => 'utf8',
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host='. env('DB_HOST') .';dbname='. env('DB_NAME'),
+    'dsn' => 'mysql:host=' . env('DB_HOST') . ';dbname=' . env('DB_NAME'),
     'username' => env('DB_USER'),
     'password' => env('DB_PASS'),
     'charset' => env('CHARSET'),

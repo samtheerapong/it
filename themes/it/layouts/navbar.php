c1eb1af793723ce8e691c83a6376d7b91d7adfe6
samtheerapong <theerapong.khan@gmail.com>
2024-02-14 17:06:33 +0700

    up

--- themes/it/layouts/navbar.php
+++ themes/it/layouts/navbar.php
@@ -13,52 +13,25 @@ use yii\helpers\Url;
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="<?= Url::toRoute('/site/index') ?>" class="nav-link"><?= Yii::t('app', 'Home') ?></a>
+            <a href="<?= Url::toRoute('/site/index') ?>" class="nav-link"><?= Yii::t('app', 'Home ') ?></a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="<?= Url::toRoute('/it/todo/index') ?>" class="nav-link"><?= Yii::t('app', 'Todos') ?></a>
-        </li>
-       
+      
+
         <li class="nav-item dropdown">
-            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle"><?= Yii::t('app', 'Config') ?></a>
-            <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-                <li><a href="<?= Url::toRoute('/config/department/index') ?>" class="dropdown-item"><?= Yii::t('app', 'Department') ?></a></li>
-                <li><a href="<?= Url::toRoute('/config/hardware/index') ?>" class="dropdown-item"><?= Yii::t('app', 'Hardware') ?></a></li>
-                <li><a href="<?= Url::toRoute('/config/software/index') ?>" class="dropdown-item"><?= Yii::t('app', 'Software') ?></a></li>
-                <li><a href="<?= Url::toRoute('/config/todo-status/index') ?>" class="dropdown-item"><?= Yii::t('app', 'Status') ?></a></li>
-                <!-- <li class="dropdown-divider"></li>
-                <li><a href="<?= Url::toRoute('/user/index') ?>" class="dropdown-item"><?= Yii::t('app', 'User') ?></a></li> -->
-            </ul>
-        </li>
-        <li class="nav-item dropdown">
-            <a id="dropdownlogout" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Dropdown</a>
-            <ul aria-labelledby="dropdownlogout" class="dropdown-menu border-0 shadow">
-                <li><a href="#" class="dropdown-item">Some action </a></li>
-                <li><a href="#" class="dropdown-item">Some other action</a></li>
-                <li><?= Html::a('Sign out', ['site/logout'], ['data-method' => 'post', 'class' => 'dropdown-item']) ?></li>
+            <a id="todolv01" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">IT</a>
+            <ul aria-labelledby="todolv01" class="dropdown-menu border-0 shadow">
+                <li> <a href="<?= Url::toRoute('/it/todo/index') ?>" class="nav-link"><?= Yii::t('app', 'To do') ?></a></li>
 
                 <li class="dropdown-divider"></li>
 
                 <!-- Level two dropdown-->
                 <li class="dropdown-submenu dropdown-hover">
-                    <a id="dropdownSubMenu02" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Hover for action</a>
-                    <ul aria-labelledby="dropdownSubMenu02" class="dropdown-menu border-0 shadow">
-                        <li>
-                            <a tabindex="-1" href="#" class="dropdown-item">level 2</a>
-                        </li>
-
-                        <!-- Level three dropdown-->
-                        <li class="dropdown-submenu">
-                            <a id="dropdownSubMenu03" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">level 2</a>
-                            <ul aria-labelledby="dropdownSubMenu03" class="dropdown-menu border-0 shadow">
-                                <li><a href="#" class="dropdown-item">3rd level</a></li>
-                                <li><a href="#" class="dropdown-item">3rd level</a></li>
-                            </ul>
-                        </li>
-                        <!-- End Level three -->
-
-                        <li><a href="#" class="dropdown-item">level 2</a></li>
-                        <li><a href="#" class="dropdown-item">level 2</a></li>
+                    <a id="todolv02" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Config</a>
+                    <ul aria-labelledby="todolv02" class="dropdown-menu border-0 shadow">
+                        <li><a href="<?= Url::toRoute('/config/department/index') ?>" class="dropdown-item"><?= Yii::t('app', 'Department') ?></a></li>
+                        <li><a href="<?= Url::toRoute('/config/hardware/index') ?>" class="dropdown-item"><?= Yii::t('app', 'Hardware') ?></a></li>
+                        <li><a href="<?= Url::toRoute('/config/software/index') ?>" class="dropdown-item"><?= Yii::t('app', 'Software') ?></a></li>
+                        <li><a href="<?= Url::toRoute('/config/todo-status/index') ?>" class="dropdown-item"><?= Yii::t('app', 'Status') ?></a></li>
                     </ul>
                 </li>
                 <!-- End Level two -->
